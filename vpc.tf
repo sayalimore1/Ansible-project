15845ae6610dbaa0dc7ca3f89bfb7595c4f8faac
sayalimore1 <sayalim@epiqinfo.com>
2023-04-28 19:01:48 +0530

    sayali

---
+++ vpc.tf
@@ -0,0 +1,18 @@
+resource "aws_vpc" "VPC_TFANS_Demo" {
+
+cidr_block       = "10.0.0.0/16"
+
+  instance_tenancy = "default"
+
+
+ tags = {
+
+    Name = "ansible_tf_Project"
+
+    Purpose = "Demo_Project"
+
+    Owner = "SayliM"
+
+  }
+
+}
